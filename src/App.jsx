dbbb7a1fad1cba3ea8624ae84a5b72442cf69e39
Dongato <developermrcat@gmail.com>
2023-04-29 22:26:40 -0600

    installation completed

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { useSelector } from 'react-redux';
+import TaskForm from './components/TaskForm';
+import TasksList from './components/TasksList';
+
+function App() {
+  const tasksState = useSelector(state => state.tasks);
+  console.log(tasksState);
+
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <TaskForm />
+      <TasksList/>
+    </div>
+  );
+}
+
+export default App;
