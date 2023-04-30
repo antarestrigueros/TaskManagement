075cc1ae7f33b3081ad147a842cff570ba2664e3
Dongato <developermrcat@gmail.com>
2023-04-30 05:54:54 -0600

    finished task logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css';
-import TaskForm from './components/TaskForm';
-import TasksList from './components/TasksList';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import TaskForm from "./components/TaskForm";
+import TasksList from "./components/TasksList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <TaskForm />
-      <TasksList/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TasksList />} />
+          <Route path="/create-task" element={<TaskForm />} />
+          <Route path="/edit-task/:id" element={<TaskForm />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,25 +1,32 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { deleteTask } from '../features/tasks/taskSlice';
+import { useSelector, useDispatch } from "react-redux";
+import { deleteTask } from "../features/tasks/taskSlice";
+import { Link } from "react-router-dom";
 
 function TasksList() {
-  const tasks = useSelector(state => state.tasks);
+  const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
-  
+
   const handleDelete = (id) => {
     dispatch(deleteTask(id));
   };
 
   return (
     <div>
-      {tasks.map(task => (
+      <header>
+        <h1>Tasks 10 {tasks.length}</h1>
+        <Link to="/create-task">Create Task</Link>
+      </header>
+
+      {tasks.map((task) => (
         <div key={task.id}>
           <h3>{task.title}</h3>
           <p>{task.description}</p>
           <button onClick={() => handleDelete(task.id)}>Delete</button>
+          <Link to={`/edit-task/${task.id}`}>Edit</Link>
         </div>
       ))}
     </div>
   );
-};
+}
 
 export default TasksList;
