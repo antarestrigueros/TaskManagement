0ee54008219e7a57d130e67c777edb33c08ab079
Dongato <developermrcat@gmail.com>
2023-04-30 07:02:35 -0600

    test style

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -46,21 +46,25 @@ function TaskForm() {
   }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4">
+      <label htmlFor="tittle" className="block text-sm font-bold mb-2">Task:</label>
       <input
         name="title"
         type="text"
         placeholder="title"
         onChange={handleChange}
         value={task.title}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       />
+      <label htmlFor="description" className="block text-sm font-bold mb-2">Description:</label>
       <textarea
         name="description"
         placeholder="description"
         onChange={handleChange}
         value={task.description}
+        className="w-full p-2 rounded-md bg-zinc-600 mb-2"
       ></textarea>
-      <button>Save</button>
+      <button className="bg-indigo-600 px-2 py-1">Save</button>
     </form>
   );
 }
